728f8f4301b74daa75cc0d18dd21e1dab3625144
abhingupta <abhin@outlook.com>
2018-10-07 01:19:50 -0400

    bug fix

--- app/environment.default.php
+++ app/environment.default.php
@@ -1,13 +1,5 @@
 <?php
 
-// Change the working directory to this file
-chdir(__DIR__);
-set_include_path(__DIR__);
-if($_SERVER['REQUEST_METHOD']=='POST'&& stripos($_SERVER['CONTENT_TYPE'],'application/json')!== false){
-  $_POST= json_decode(file_get_contents('php://input'),true);
-}
-require 'environment.php';
-
-//MODELS
-require 'models/Work.php';
-require 'models/Team.php';
+const DB_USER = 'user';
+const DB_PW = 'pw';
+const DB_NAME = 'hello';
