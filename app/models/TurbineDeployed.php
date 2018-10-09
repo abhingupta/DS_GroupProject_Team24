a080366fcf00897bc7d92bfaf727dc7fe632513c
Alix Faulkner <anfaulkn@iu.edu>
2018-10-09 11:48:49 -0400

    creating the database and SQL connection

--- app/models/TurbineDeployed.php
+++ app/models/TurbineDeployed.php
@@ -23,3 +23,24 @@ class TurbineDeployed{
     $this->lastPlannedOutageDate = $row['lastPlannedOutageDate'];
     $this->lastUnplannedOutageDate = $row['lastUnplannedOutageDate'];
   }
+
+  public function create(){
+  $db = new PDO(DB_SEVER, DB_USER, DB_PW);
+  $sql = 'INSERT INTO turbine_deployed(turbineDeployedId,turbineId,siteId,serialNumber,deployedDate,totalFiredHours,totalStarts,lastPlannedOutageDate,lastUnplannedOutageDate) VALUES (?,?,?,?,?,?,?,?,?)';
+
+  $statement = $db->prepare($sql);
+  $success = $statement->execute([
+  $this-> turbineDeployedId,
+  $this-> turbineId,
+  $this-> siteId,
+  $this-> serialNumber,
+  $this-> deployedDate,
+  $this-> totalFiredHours,
+  $this-> totalStarts,
+  $this-> lastPlannedOutageDate,
+  $this-> lastUnplannedOutageDate]);
+  if(!$success){
+    die('bad sql on insert');
+  }
+  $this->turbineDeployedId = $db->lastInsertId();
+}
