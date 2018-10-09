ee1f222888fe08ad29a570de3ac5a697b174505f
abhingupta <abhin@outlook.com>
2018-10-09 13:44:41 -0400

    updated common.php

--- app/common.php
+++ app/common.php
@@ -11,7 +11,7 @@ require 'environment.php';
 //MODELS
 require 'models/Client.php';
 require 'models/Sensor.php';
-require 'models/Turbine.php';
+// require 'models/Turbine.php';
 require 'models/TurbineDeployed.php';
 require 'models/SensorDeployed.php';
 require 'models/SensorTimeSeries.php';
