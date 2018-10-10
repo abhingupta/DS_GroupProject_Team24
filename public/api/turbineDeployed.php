3dae3180bf2a2692cc4e10f9e7266b5cdaf0f56f
abhingupta <abhin@outlook.com>
2018-10-09 21:08:00 -0400

    wrong bug before

--- public/api/turbineDeployed.php
+++ public/api/turbineDeployed.php
@@ -2,16 +2,16 @@
 
 require '../../app/common.php';
 
-// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//   require 'turbineDeployedPost.php';
-//   exit;
-// }
-
-$turbineDeployedId = intval($_GET['turbineDeployedId'] ?? 0);
-
-if ($turbineDeployedId < 1) {
-  throw new Exception('Invalid TurbineDeployed ID');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  require 'turbineDeployedPost.php';
+  exit;
 }
+
+// $turbineDeployedId = intval($_GET['turbineDeployedId'] ?? 0);
+//
+// if ($turbineDeployedId < 1) {
+//   throw new Exception('Invalid TurbineDeployed ID');
+// }
 
 // 1. Go to the database and get all work associated with the $turbineDeployedId
 // $turbineDeployedArr = TurbineDeployed::getTurbineDeployedById($turbineDeployedId);
