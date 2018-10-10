623a56d4bf06b4456b06e61378dbb1fe77d2d48a
abhingupta <abhin@outlook.com>
2018-10-09 21:05:40 -0400

    space bug

--- app/models/TurbineDeployed.php
+++ app/models/TurbineDeployed.php
@@ -63,9 +63,7 @@ class TurbineDeployed{
   $sql = 'SELECT * FROM turbine_deployed;
   $statement = $db->prepare($sql);
   // 3. Run the query
-  $success = $statement->execute(
-      
-  );
+  $success = $statement->execute();
 
  // 4. Handle the results
  $arr = [];
