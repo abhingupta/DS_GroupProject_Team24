1c7c3e6c1e4d52f6d43f1ca924c6d35d5a891d49
abhingupta <abhin@outlook.com>
2018-10-11 19:02:25 -0400

    consolidated data

--- public/api/fetchAll.php
+++ public/api/fetchAll.php
@@ -13,13 +13,39 @@ require '../../app/common.php';
 
 $turbineDeployedArr = TurbineDeployed::getTurbineDeployedById();
 // 2. Convert to JSON
-$json = json_encode($turbineDeployedArr, JSON_PRETTY_PRINT);
+$turbineDeployedJson = json_encode($turbineDeployedArr, JSON_PRETTY_PRINT);
 
-// $turbineDeployedArr1 = TurbineDeployed::getTurbineDeployedById();
-// // 2. Convert to JSON
-// $json1 = json_encode($turbineDeployedArr1, JSON_PRETTY_PRINT);
+$clientArr = Client::getClientById();
 
+// 2. Convert to JSON
+$clientJson = json_encode($clientArr, JSON_PRETTY_PRINT);
+
+$sensorArr = Sensor::getSensorById();
+
+// 2. Convert to JSON
+$sensorJson = json_encode($sensorArr, JSON_PRETTY_PRINT);
+
+$sensorDeployedArr = SensorDeployed::getSensorDeployedById();
+
+// 2. Convert to JSON
+$sensorDeployedJson = json_encode($sensorDeployedArr, JSON_PRETTY_PRINT);
+
+$sensorTimeSeriesArr = SensorTimeSeries::SensorTimeSeriesById();
+
+// 2. Convert to JSON
+$sensorTimeSeriesjson = json_encode($sensorTimeSeriesArr, JSON_PRETTY_PRINT);
+
+$siteArr = Site::getSiteById();
+
+// 2. Convert to JSON
+$siteJson = json_encode($siteArr, JSON_PRETTY_PRINT);
+
+
+$turbineArr = Turbine::getTurbineById();
+
+// 2. Convert to JSON
+$turbineJson = json_encode($turbineArr, JSON_PRETTY_PRINT);
 // 3. Print
 header('Content-Type: application/json');
-echo $json;
-// echo $json . "nl2br" . "New array" . $json1 .  "\n";
+// echo $json;
+ echo $siteJson . "\n" . "Site data" . $clientJson .  "\n". "Turbine data" . $turbineJson .  "\n". "Sensor data" . $sensorJson .  "\n". "Sensor Deployed data" . $sensorDeployedJson .  "\n". "Turbine deployed data" . $turbineDeployedJson .  "\n". "Sensor Time series data" . $sensorTimeSeriesjson .  "\n";
