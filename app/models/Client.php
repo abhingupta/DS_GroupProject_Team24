9b1a7566989fcb66fc5969f1367f41b0e9b89691
abhingupta <abhin@outlook.com>
2018-10-11 19:35:06 -0400

    bug

--- app/models/Client.php
+++ app/models/Client.php
@@ -37,27 +37,19 @@ class Client{
    // 1. Connect to the database
    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
    // 2. Prepare the query
-   $sql = 'SELECT * FROM client ';
+   $sql = 'SELECT * FROM client';
    $statement = $db->prepare($sql);
    // 3. Run the query
    $success = $statement->execute();
 
-   // public static function getClientById() {
-   //  // 1. Connect to the database
-   //  $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-   //  // 2. Prepare the query
-   //  $sql = 'SELECT * FROM client';
-   //  $statement = $db->prepare($sql);
-   //  // 3. Run the query
-   //  $success = $statement->execute();
-   // 4. Handle the results
-   $arr = [];
-   while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-     // 4.a. For each row, make a new work object
-     $clientItem =  new Client($row);
-     array_push($arr, $clientItem);
-   }
-   // 4.b. return the array of work objects
-   return $arr;
+  // 4. Handle the results
+  $arr = [];
+  while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    // 4.a. For each row, make a new work object
+    $clientItem =  new Client($row);
+    array_push($arr, clientItem);
+  }
+  // 4.b. return the array of work objects
+  return $arr;
  }
 }
