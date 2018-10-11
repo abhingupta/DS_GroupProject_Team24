f442f1e2ce377c5ec647c2277c27a6acc7c7a2c1
abhingupta <abhin@outlook.com>
2018-10-11 19:39:06 -0400

    one with client id

--- public/api/client.php
+++ public/api/client.php
@@ -6,16 +6,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'clientPost.php';
   exit;
 }
-//
-// $clientId = intval($_GET['clientId'] ?? 0);
-//
-// if ($clientId < 1) {
-//   throw new Exception('Invalid Client ID');
-// }
+
+$clientId = intval($_GET['clientId'] ?? 0);
+
+if ($clientId < 1) {
+  throw new Exception('Invalid Client ID');
+}
 
 
 // 1. Go to the database and get all work associated with the $taskId
-$clientArr = Client::getClientById();
+$clientArr = Client::getClientById($clientId);
 
 // 2. Convert to JSON
 $json = json_encode($clientArr, JSON_PRETTY_PRINT);
