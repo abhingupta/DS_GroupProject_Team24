db48d19527ac079309877b4a2227e580ff23fad4
abhingupta <abhin@outlook.com>
2018-10-24 09:51:55 -0400

    trying a bug fix

--- public/api/client1.php
+++ public/api/client1.php
@@ -15,10 +15,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
 // 1. Go to the database and get all work associated with the $taskId
-$clientArr = Client1::getClientById();
+$clientArr1 = Client1::getClientById();
 
 // 2. Convert to JSON
-$json = json_encode($clientArr, JSON_PRETTY_PRINT);
+$json = json_encode($clientArr1, JSON_PRETTY_PRINT);
 
 // 3. Print
 header('Content-Type: application/json');
