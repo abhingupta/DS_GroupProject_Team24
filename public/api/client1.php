c6e0d38db6abd221dd5f49cf16336e99b476e6b2
abhingupta <abhin@outlook.com>
2018-10-24 11:53:19 -0400

    trying decode

--- public/api/client1.php
+++ public/api/client1.php
@@ -18,8 +18,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 $clientArr1 = Client1::getClientById();
 
 // 2. Convert to JSON
-$json = json_encode($clientArr1, JSON_PRETTY_PRINT);
+$json = json_decode($clientArr1, JSON_PRETTY_PRINT);
 
 // 3. Print
 header('Content-Type: application/json');
-echo $clientArr1;
+echo $json;
