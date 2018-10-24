64ae6f830bcaee372716b5df06326a9bc79fd73a
abhingupta <abhin@outlook.com>
2018-10-24 00:23:14 -0400

    basic structure

---
+++ public/js/site.app.js
@@ -0,0 +1,92 @@
+var siteApp = new Vue({
+	el: '#siteMain',
+	data: {
+		siteData: {
+			siteId: null,
+			siteName: 'foo',
+			siteDescription: 'foo',
+			gicsSector: 'foo',
+			gicsSubIndustry: 'foo',
+			headquarter: 'foo'
+		},
+		site: [],
+		siteForm: {}, // populated by this.getEmptySiteForm()
+	},
+	computed: {
+
+	},
+	methods: {
+		handleSiteForm(e) {
+			const s = JSON.stringify(this.siteForm);
+
+			console.log(s);
+
+			// POST to remote server
+			fetch('api/site.php', {
+					method: "POST", // *GET, POST, PUT, DELETE, etc.
+					headers: {
+						"Content-Type": "application/json; charset=utf-8"
+					},
+					body: s // body data type must match "Content-Type" header
+				})
+				.then(response => response.json())
+				.then(json => {
+					this.site.push(json)
+				})
+				.catch(err => {
+					console.error('Site POST ERROR:');
+					console.error(err);
+				})
+
+			// Reset siteForm
+			this.siteForm = this.getEmptySiteForm();
+		},
+
+
+		getEmptySiteForm() {
+			return {
+				// site_id: this.siteData.id,
+				// siteIdnull,
+				siteName: null,
+				siteDescription: null,
+				gicsSector: null,
+				gicsSubIndustry: null,
+				headquarter: null
+
+			}
+		},
+		gotoSite(tid) {
+			console.log("site id:" +
+				tid);
+			window.location = 'site.html?siteId=' + tid;
+		}
+	},
+	created() {
+
+		// Do data fetch
+		const url = new URL(window.location.href);
+		const siteId = url.searchParams.get('siteId');
+		console.log('Site: ' + siteId);
+		this.siteData.id = siteId;
+
+		if (!siteId) {
+			//TODO: Error? 404?
+			//e.g., window.location = '404.html';
+		}
+
+		// Populate siteForm with default values
+		this.siteForm = this.getEmptySiteForm();
+
+		// TODO: Fetch task-specific data
+		// fetch('api/task?id=4')
+		fetch('api/site.php?siteId=' + siteId)
+			.then(response => response.json())
+			.then(json => {
+				siteApp.site = json
+			})
+			.catch(err => {
+				console.error('Site FETCH ERROR:');
+				console.error(err);
+			})
+	}
+})
