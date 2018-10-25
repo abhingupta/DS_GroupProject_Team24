b927216e2a686383e07cbdbc0a96bfc8879bdf53
abhingupta <abhin@outlook.com>
2018-10-25 13:27:54 -0400

    revert

--- app/models/Client1.php
+++ app/models/Client1.php
@@ -61,17 +61,7 @@ class Client1{
       die('bad sql on insert');
     }
 
-    public function update() {
-     // 1. Connect to the database
-     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-     // 2. Prepare the query
-     $sql = 'UPDATE  client INSERT INTO notes WHERE clientId=?';
-     $statement = $db->prepare($sql);
-     // 3. Run the query
-     $success = $statement->execute();
-     if(!$success){
-       die('bad sql on insert');
-     }
+  
    // 4. Handle the results
    $arr = [];
    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
