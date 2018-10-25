5e8928eb3e689641f94ce2f2135b0b808f01335b
abhingupta <abhin@outlook.com>
2018-10-25 13:25:50 -0400

    update notes

--- app/models/Client1.php
+++ app/models/Client1.php
@@ -60,6 +60,18 @@ class Client1{
     if(!$success){
       die('bad sql on insert');
     }
+
+    public static function update() {
+     // 1. Connect to the database
+     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+     // 2. Prepare the query
+     $sql = 'UPDATE  client INSERT INTO notes WHERE clientId=?';
+     $statement = $db->prepare($sql);
+     // 3. Run the query
+     $success = $statement->execute();
+     if(!$success){
+       die('bad sql on insert');
+     }
    // 4. Handle the results
    $arr = [];
    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
