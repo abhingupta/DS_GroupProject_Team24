39f4485a988b026cc000871474c67bcf178f166b
abhingupta <abhin@outlook.com>
2018-10-24 21:52:02 -0400

    encoding array utf8

--- public/api/client1.php
+++ public/api/client1.php
@@ -19,45 +19,51 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 $hello = Client1::getClientById();
 
 // 2. Convert to JSON
-$json1 = json_encode($hello, JSON_PRETTY_PRINT);
+foreach($hello as &$v) {
+  $v['order_item_name'] = utf8_encode($v['order_item_name']);
+}
+
+// $json1 = json_encode($hello, JSON_PRETTY_PRINT);
 
 // $error = json_last_error();
 
-foreach ($hello as $string) {
-    echo 'Encoding: ' . $string;
-    json_encode($string);
+// foreach ($hello as $string) {
+//     echo 'Encoding: ' . $string;
+//     json_encode($string);
 
-    switch (json_last_error()) {
-        case JSON_ERROR_NONE:
-            echo ' - No errors';
-        break;
-        case JSON_ERROR_DEPTH:
-            echo ' - Maximum stack depth exceeded';
-        break;
-        case JSON_ERROR_STATE_MISMATCH:
-            echo ' - Underflow or the modes mismatch';
-        break;
-        case JSON_ERROR_CTRL_CHAR:
-            echo ' - Unexpected control character found';
-        break;
-        case JSON_ERROR_SYNTAX:
-            echo ' - Syntax error, malformed JSON';
-        break;
-        case JSON_ERROR_UTF8:
-            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
-        break;
-        default:
-            echo ' - Unknown error';
-        break;
-    }
-
-    echo PHP_EOL;
-}
+//     switch (json_last_error()) {
+//         case JSON_ERROR_NONE:
+//             echo ' - No errors';
+//         break;
+//         case JSON_ERROR_DEPTH:
+//             echo ' - Maximum stack depth exceeded';
+//         break;
+//         case JSON_ERROR_STATE_MISMATCH:
+//             echo ' - Underflow or the modes mismatch';
+//         break;
+//         case JSON_ERROR_CTRL_CHAR:
+//             echo ' - Unexpected control character found';
+//         break;
+//         case JSON_ERROR_SYNTAX:
+//             echo ' - Syntax error, malformed JSON';
+//         break;
+//         case JSON_ERROR_UTF8:
+//             echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+//         break;
+//         default:
+//             echo ' - Unknown error';
+//         break;
+//     }
+//
+//     echo PHP_EOL;
+// }
 
 
 // 3. Print
 header('Content-Type: application/json');
-echo $json1;
-echo '<pre> buga buga1 buga';
-var_dump($json1);
-echo '</pre>';
+print json_encode($hello);
+
+// echo $json1;
+// echo '<pre> buga buga1 buga';
+// var_dump($json1);
+// echo '</pre>';
