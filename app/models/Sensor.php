e7b198a6a08df37215ff3b55ddb94be98b8e2896
abhingupta <abhin@outlook.com>
2018-10-25 15:40:59 -0400

    bug fix

--- app/models/Sensor.php
+++ app/models/Sensor.php
@@ -35,7 +35,7 @@ class Sensor{
    // 1. Connect to the database
    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
    // 2. Prepare the query
-   $sql = 'SELECT * FROM sensor WHERE sensorDeployedId = ?';
+   $sql = 'SELECT * FROM sensor WHERE sensorId = ?';
    $statement = $db->prepare($sql);
    // 3. Run the query
    $success = $statement->execute(
