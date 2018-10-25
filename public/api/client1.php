e2e7a32ff59342cc3423f6ecd249f386de6a6c3e
abhingupta <abhin@outlook.com>
2018-10-24 23:13:55 -0400

    revert

--- public/api/client1.php
+++ public/api/client1.php
@@ -24,8 +24,8 @@ $hello = Client1::getClientById();
 // }
 
 $json1 = json_encode($hello, JSON_PRETTY_PRINT);
-$json = json_last_error_msg();
-echo($json);
+// $json = json_last_error_msg();
+// echo($json);
 // $error = json_last_error();
 
 // foreach ($hello as $string) {
