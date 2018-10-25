54670fda23bb2c419ed1e4896ff097993140838c
abhingupta <abhin@outlook.com>
2018-10-25 14:58:29 -0400

    big fix

--- app/models/Sensor.php
+++ app/models/Sensor.php
@@ -31,26 +31,26 @@ class Sensor{
     }
     $this->sensorId = $db->lastInsertId();
   }
-  // public static function getSensorById(int $sensorId) {
-  //  // 1. Connect to the database
-  //  $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-  //  // 2. Prepare the query
-  //  $sql = 'SELECT * FROM sensor WHERE sensorId = ?';
-  //  $statement = $db->prepare($sql);
-  //  // 3. Run the query
-  //  $success = $statement->execute(
-  //      [$sensorId]
-  //  );
+  public static function getSensorById(int $sensorId) {
+   // 1. Connect to the database
+   $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+   // 2. Prepare the query
+   $sql = 'SELECT * FROM sensor WHERE sensorId = ?';
+   $statement = $db->prepare($sql);
+   // 3. Run the query
+   $success = $statement->execute(
+       [$sensorId]
+   );
 
-   public static function getSensorById() {
-    // 1. Connect to the database
-    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-    // 2. Prepare the query
-    $sql = 'SELECT * FROM sensor';
-    $statement = $db->prepare($sql);
-    // 3. Run the query
-    $success = $statement->execute();
-   // 4. Handle the results
+   // public static function getSensorById() {
+   //  // 1. Connect to the database
+   //  $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+   //  // 2. Prepare the query
+   //  $sql = 'SELECT * FROM sensor';
+   //  $statement = $db->prepare($sql);
+   //  // 3. Run the query
+   //  $success = $statement->execute();
+   // // 4. Handle the results
    $arr = [];
    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
      // 4.a. For each row, make a new work object
--- public/api/sensor.php
+++ public/api/sensor.php
@@ -7,15 +7,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 
-// $sensorId = intval($_GET['sensorId'] ?? 0);
-//
-// if ($sensorId < 1) {
-//   throw new Exception('Invalid Sensor ID');
-// }
+$sensorId = intval($_GET['sensorId'] ?? 0);
+
+if ($sensorId < 1) {
+  throw new Exception('Invalid Sensor ID');
+}
 
 
 // 1. Go to the database and get all work associated with the $taskId
-$sensorArr = Sensor::getSensorById();
+$sensorArr = Sensor::getSensorById($sensorId);
 
 // 2. Convert to JSON
 $json = json_encode($sensorArr, JSON_PRETTY_PRINT);
