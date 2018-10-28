d31038407f0175b7dc9d40a5315aec5e7999545c
abhingupta <abhin@outlook.com>
2018-10-27 22:08:46 -0400

    update

--- public/js/siteTest.app.js
+++ public/js/siteTest.app.js
@@ -1,32 +1,8 @@
-Vue.directive('expand', {
-	inserted: function (el, binding) {
-		if (binding.value !== null) {
-			function calcHeight() {
-				const currentState = el.getAttribute('aria-expanded')
+import {
+	Collapse
+} from 'bootstrap-vue/es/components';
 
-				el.classList.add('u-no-transition')
-				el.removeAttribute('aria-expanded')
-				el.style.height = null
-				el.style.height = el.clientHeight + 'px'
-				el.setAttribute('aria-expanded', currentState)
-
-				setTimeout(function () {
-					el.classList.remove('u-no-transition')
-				})
-			}
-
-			el.classList.add('expand')
-			el.setAttribute('aria-expanded', (binding.value) ? 'true' : 'false')
-			calcHeight()
-			window.addEventListener('resize', calcHeight)
-		}
-	},
-	update: function (el, binding) {
-		if (el.style.height && binding.value !== null) {
-			el.setAttribute('aria-expanded', (binding.value) ? 'true' : 'false')
-		}
-	},
-})
+Vue.use(Collapse);
 
 var siteApp = new Vue({
 	el: '#siteMain',
