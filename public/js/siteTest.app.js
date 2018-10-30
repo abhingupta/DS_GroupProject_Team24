d80959c636e5566514e63704d023b7c2dd9a7a97
abhingupta <abhin@outlook.com>
2018-10-30 11:53:51 -0400

    test

--- public/js/siteTest.app.js
+++ public/js/siteTest.app.js
@@ -442,70 +442,72 @@ var siteApp = new Vue({
 				}]
 			});
 		},
-		buildAverageChart() {
-			renderIcons() {
 
-				// Move icon
-				if (!this.series[0].icon) {
-					this.series[0].icon = this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
-						.attr({
-							'stroke': '#303030',
-							'stroke-linecap': 'round',
-							'stroke-linejoin': 'round',
-							'stroke-width': 2,
-							'zIndex': 10
-						})
-						.add(this.series[2].group);
-				}
-				this.series[0].icon.translate(
-					this.chartWidth / 2 - 10,
-					this.plotHeight / 2 - this.series[0].points[0].shapeArgs.innerR -
-					(this.series[0].points[0].shapeArgs.r - this.series[0].points[0].shapeArgs.innerR) / 2
-				);
+		renderIcons() {
 
-				// Exercise icon
-				if (!this.series[1].icon) {
-					this.series[1].icon = this.renderer.path(
-							['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8,
-								'M', 8, -8, 'L', 16, 0, 8, 8
-							]
-						)
-						.attr({
-							'stroke': '#ffffff',
-							'stroke-linecap': 'round',
-							'stroke-linejoin': 'round',
-							'stroke-width': 2,
-							'zIndex': 10
-						})
-						.add(this.series[2].group);
-				}
-				this.series[1].icon.translate(
-					this.chartWidth / 2 - 10,
-					this.plotHeight / 2 - this.series[1].points[0].shapeArgs.innerR -
-					(this.series[1].points[0].shapeArgs.r - this.series[1].points[0].shapeArgs.innerR) / 2
-				);
+			// Move icon
+			if (!this.series[0].icon) {
+				this.series[0].icon = this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
+					.attr({
+						'stroke': '#303030',
+						'stroke-linecap': 'round',
+						'stroke-linejoin': 'round',
+						'stroke-width': 2,
+						'zIndex': 10
+					})
+					.add(this.series[2].group);
+			}
+			this.series[0].icon.translate(
+				this.chartWidth / 2 - 10,
+				this.plotHeight / 2 - this.series[0].points[0].shapeArgs.innerR -
+				(this.series[0].points[0].shapeArgs.r - this.series[0].points[0].shapeArgs.innerR) / 2
+			);
 
-				// Stand icon
-				if (!this.series[2].icon) {
-					this.series[2].icon = this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
-						.attr({
-							'stroke': '#303030',
-							'stroke-linecap': 'round',
-							'stroke-linejoin': 'round',
-							'stroke-width': 2,
-							'zIndex': 10
-						})
-						.add(this.series[2].group);
-				}
+			// Exercise icon
+			if (!this.series[1].icon) {
+				this.series[1].icon = this.renderer.path(
+						['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8,
+							'M', 8, -8, 'L', 16, 0, 8, 8
+						]
+					)
+					.attr({
+						'stroke': '#ffffff',
+						'stroke-linecap': 'round',
+						'stroke-linejoin': 'round',
+						'stroke-width': 2,
+						'zIndex': 10
+					})
+					.add(this.series[2].group);
+			}
+			this.series[1].icon.translate(
+				this.chartWidth / 2 - 10,
+				this.plotHeight / 2 - this.series[1].points[0].shapeArgs.innerR -
+				(this.series[1].points[0].shapeArgs.r - this.series[1].points[0].shapeArgs.innerR) / 2
+			);
 
-				this.series[2].icon.translate(
-					this.chartWidth / 2 - 10,
-					this.plotHeight / 2 - this.series[2].points[0].shapeArgs.innerR -
-					(this.series[2].points[0].shapeArgs.r - this.series[2].points[0].shapeArgs.innerR) / 2
-				);
+			// Stand icon
+			if (!this.series[2].icon) {
+				this.series[2].icon = this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
+					.attr({
+						'stroke': '#303030',
+						'stroke-linecap': 'round',
+						'stroke-linejoin': 'round',
+						'stroke-width': 2,
+						'zIndex': 10
+					})
+					.add(this.series[2].group);
 			}
 
-			Highcharts.chart('averageCharts', {
+			this.series[2].icon.translate(
+				this.chartWidth / 2 - 10,
+				this.plotHeight / 2 - this.series[2].points[0].shapeArgs.innerR -
+				(this.series[2].points[0].shapeArgs.r - this.series[2].points[0].shapeArgs.innerR) / 2
+			);
+		},
+		buildAverageChart() {
+
+
+			Highcharts.chart('container', {
 
 				chart: {
 					type: 'solidgauge',
@@ -687,6 +689,7 @@ var siteApp = new Vue({
 			this.buildHeatrateChart();
 			this.buildCompressorChart();
 			// this.buildAvalibilityChart();
+			this.renderIcons();
 			this.buildAverageChart();
 			this.buildReliabilityChart();
 			this.buildHybridChart();
