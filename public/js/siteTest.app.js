00221bee5805206a64c14e144c2b7543e9bee98b
abhingupta <abhin@outlook.com>
2018-10-30 11:44:11 -0400

    revert

--- public/js/siteTest.app.js
+++ public/js/siteTest.app.js
@@ -263,169 +263,64 @@ var siteApp = new Vue({
 
 
 		buildAvalibilityChart() {
-			/*
-The purpose of this demo is to demonstrate how multiple charts on the same page
-can be linked through DOM and Highcharts events and API methods. It takes a
-standard Highcharts config with a small variation for each data set, and a
-mouse/touch event handler to bind the charts together.
-*/
+			Highcharts.chart('availabilityCharts', {
+				chart: {
+					backgroundColor: 'transparent'
+				},
+				title: {
+					style: {
+						// fontFamily: 'monospace',
+						color: '#ffffff',
+						fontWeight: 'bold'
+					},
+					text: 'Availability Chart'
+				},
 
-
-
-			/**
-			 * In order to synchronize tooltips and crosshairs, override the
-			 * built-in events with handlers defined on the parent element.
-			 */
-			['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType) {
-				document.getElementById('availabilityCharts').addEventListener(
-					eventType,
-					function (e) {
-						var chart,
-							point,
-							i,
-							event;
-
-						for (i = 0; i < Highcharts.charts.length; i = i + 1) {
-							chart = Highcharts.charts[i];
-							// Find coordinates within the chart
-							event = chart.pointer.normalize(e);
-							// Get the hovered point
-							point = chart.series[0].searchPoint(event, true);
-
-							if (point) {
-								point.highlight(e);
-							}
-						}
+				xAxis: {
+					type: 'datetime'
+				},
+				yAxis: {
+					title: {
+						text: 'Availability'
 					}
-				);
-			});
-
-			/**
-			 * Override the reset function, we don't need to hide the tooltips and
-			 * crosshairs.
-			 */
-			Highcharts.Pointer.prototype.reset = function () {
-				return undefined;
-			};
-
-			/**
-			 * Highlight a point by showing tooltip, setting hover state and draw crosshair
-			 */
-			Highcharts.Point.prototype.highlight = function (event) {
-				event = this.series.chart.pointer.normalize(event);
-				this.onMouseOver(); // Show the hover marker
-				this.series.chart.tooltip.refresh(this); // Show the tooltip
-				this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
-			};
-
-			/**
-			 * Synchronize zooming through the setExtremes event handler.
-			 */
-			function syncExtremes(e) {
-				var thisChart = this.chart;
-
-				if (e.trigger !== 'syncExtremes') { // Prevent feedback loop
-					Highcharts.each(Highcharts.charts, function (chart) {
-						if (chart !== thisChart) {
-							if (chart.xAxis[0].setExtremes) { // It is null while updating
-								chart.xAxis[0].setExtremes(
-									e.min,
-									e.max,
-									undefined,
-									false, {
-										trigger: 'syncExtremes'
-									}
-								);
+				},
+				legend: {
+					enabled: true
+				},
+				plotOptions: {
+					area: {
+						fillColor: {
+							linearGradient: {
+								x1: 0,
+								y1: 0,
+								x2: 0,
+								y2: 1
+							},
+							stops: [
+								[0, Highcharts.getOptions().colors[0]],
+								[1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+							]
+						},
+						marker: {
+							radius: 2
+						},
+						lineWidth: 1,
+						states: {
+							hover: {
+								lineWidth: 1
 							}
-						}
-					});
-				}
-			}
+						},
+						threshold: null
+					}
+				},
 
-			// Get the data. The contents of the data file can be viewed at
-			Highcharts.ajax({
-				url: 'https://cdn.rawgit.com/highcharts/highcharts/057b672172ccc6c08fe7dbb27fc17ebca3f5b770/samples/data/activity.json',
-				dataType: 'text',
-				success: function (activity) {
-
-					activity = JSON.parse(activity);
-					activity.datasets.forEach(function (dataset, i) {
-
-						// Add X values
-						dataset.data = Highcharts.map(dataset.data, function (val, j) {
-							return [activity.xData[j], val];
-						});
-
-						var chartDiv = document.createElement('div');
-						chartDiv.className = 'chart';
-						document.getElementById('container').appendChild(chartDiv);
-
-						Highcharts.chart(chartDiv, {
-							chart: {
-								marginLeft: 40, // Keep all charts left aligned
-								spacingTop: 20,
-								spacingBottom: 20
-							},
-							title: {
-								text: dataset.name,
-								align: 'left',
-								margin: 0,
-								x: 30
-							},
-							credits: {
-								enabled: false
-							},
-							legend: {
-								enabled: false
-							},
-							xAxis: {
-								crosshair: true,
-								events: {
-									setExtremes: syncExtremes
-								},
-								labels: {
-									format: '{value} km'
-								}
-							},
-							yAxis: {
-								title: {
-									text: null
-								}
-							},
-							tooltip: {
-								positioner: function () {
-									return {
-										// right aligned
-										x: this.chart.chartWidth - this.label.width,
-										y: 10 // align to title
-									};
-								},
-								borderWidth: 0,
-								backgroundColor: 'none',
-								pointFormat: '{point.y}',
-								headerFormat: '',
-								shadow: false,
-								style: {
-									fontSize: '18px'
-								},
-								valueDecimals: dataset.valueDecimals
-							},
-							series: [{
-								data: dataset.data,
-								name: dataset.name,
-								type: dataset.type,
-								color: Highcharts.getOptions().colors[i],
-								fillOpacity: 0.3,
-								tooltip: {
-									valueSuffix: ' ' + dataset.unit
-								}
-							}]
-						});
-					});
-				}
+				series: [{
+					type: 'area',
+					name: 'Availability(day)',
+					data: //needs to be [[date1, val1],[date2,val2]]
+						this.sensorts.map(entry => [entry.dataCollectedDate, entry.availability])
+				}]
 			});
-
-
 
 		},
 
